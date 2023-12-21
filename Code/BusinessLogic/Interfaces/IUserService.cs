e94d78827ea2d94e80004d7ae4bb487a39921146
Luis Mora <luismora02@gmail.com>
2023-12-21 12:17:07 -0500

    Add UserService + Unit Tests

---
+++ Code/BusinessLogic/Interfaces/IUserService.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogic.Models;
+
+namespace BusinessLogic.Interfaces
+{
+    /// <summary>
+    /// Define behaviors related to Users
+    /// </summary>
+    public interface IUserService
+    {
+        /// <summary>
+        /// Adds a User to the system
+        /// </summary>
+        /// <param name="entity">User info</param>
+        Task Add(User entity);
+
+        /// <summary>
+        /// Validates info to allow the user to log in to the system
+        /// </summary>
+        /// <param name="email">User email</param>
+        /// <param name="password">User password</param>
+        /// <returns>User info</returns>
+        Task<User> LogIn(string email, string password);
+    }
+}
---
+++ Code/BusinessLogic/UserService.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using BusinessLogic.Interfaces;
+using BusinessLogic.Models;
+using DataAccess.Repositories.Interfaces;
+
+namespace BusinessLogic
+{
+    public class UserService : IUserService
+    {
+        public const string ExistingUser = "There is already a User with the same Email";
+        public const string NonExistingUser = "This User does not exist - Create it if needed!";
+        public const string InvalidCredentials = "Email or Password are incorrect. Try again!";
+
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+
+        public UserService(
+            IMapper mapper,
+            IUserRepository userRepository)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+
+        /// <summary>
+        /// Implementation of the <see cref="IUserService.Add(User)"/>
+        /// </summary>
+        public async Task Add(User entity)
+        {
+            var existingUser = await _userRepository.GetByEmail(entity.Email);
+            if (existingUser != null)
+            {
+                throw new InvalidOperationException(ExistingUser);
+            }
+
+            var dbEntity = _mapper.Map<DataAccess.Entities.User>(entity);
+            await _userRepository.Add(dbEntity);
+        }
+
+        /// <summary>
+        /// Implementation of the <see cref="IUserService.LogIn(string, string)"/>
+        /// </summary>
+        public async Task<User> LogIn(string email, string password)
+        {
+            var dbUser = await _userRepository.GetByEmail(email);
+            if (dbUser is null)
+            {
+                throw new InvalidOperationException(NonExistingUser);
+            }
+
+            if (dbUser.Password != password)
+            {
+                throw new ArgumentException(InvalidCredentials);
+            }
+
+            return _mapper.Map<User>(dbUser);
+        }
+    }
+}
