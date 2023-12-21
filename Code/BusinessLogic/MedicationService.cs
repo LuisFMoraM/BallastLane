073731dffcccc23cdf15881695b368331a4b8619
Luis Mora <luis.mora@theoremone.co>
2023-12-20 21:47:10 -0500

    Add BusinessLogic Layer + Unit Tests

---
+++ Code/BusinessLogic/MedicationService.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using BusinessLogic.Interfaces;
+using BusinessLogic.Models;
+using DataAccess.Repositories.Interfaces;
+
+namespace BusinessLogic
+{
+    public class MedicationService : IMedicationService
+    {
+        public const string ExistingMedication = "There is already a Medication with the same Name and Brand";
+        public const string NonExistingMedication = "The Medication does not exist";
+
+        private readonly IMapper _mapper;
+        private readonly IMedicationRepository _medicationRepository;
+
+        public MedicationService(
+            IMapper mapper,
+            IMedicationRepository medicationRepository)
+        {
+            _mapper = mapper;
+            _medicationRepository = medicationRepository;
+        }
+
+        /// <summary>
+        /// Implementation of the <see cref="IMedicationService.GetAll"/>
+        /// </summary>
+        public async Task<List<Medication>> GetAll()
+        {
+            var entityList = await _medicationRepository.GetAll();
+            return _mapper.Map<List<Medication>>(entityList);
+        }
+
+        /// <summary>
+        /// Implementation of the <see cref="IMedicationService.Add(Medication)"/>
+        /// </summary>
+        public async Task Add(Medication entity)
+        {
+            var existingRecord = (await _medicationRepository.GetAll())
+                .SingleOrDefault(m => m.Name == entity.Name && m.Brand == entity.Brand);
+
+            if (existingRecord != null)
+            {
+                throw new ArgumentException(ExistingMedication);
+            }
+
+            var dbObject = _mapper.Map<DataAccess.Entities.Medication>(entity);
+            await _medicationRepository.Add(dbObject);
+        }
+
+        /// <summary>
+        /// Implementation of the <see cref="IMedicationService.Update(long, Medication)"/>
+        /// </summary>
+        public async Task Update(long id, Medication entity)
+        {
+            var existingRecord = await _medicationRepository.GetById(id);
+            if (existingRecord is null)
+            {
+                throw new ArgumentException(NonExistingMedication);
+            }
+
+            var dbObject = _mapper.Map<DataAccess.Entities.Medication>(entity);
+            await _medicationRepository.Update(dbObject);
+        }
+
+        /// <summary>
+        /// Implementation of the <see cref="IMedicationService.Delete(long)"/>
+        /// </summary>
+        public async Task Delete(long id)
+        {
+            var existingRecord = await _medicationRepository.GetById(id);
+            if (existingRecord is null)
+            {
+                throw new ArgumentException(NonExistingMedication);
+            }
+
+            await _medicationRepository.Delete(existingRecord);
+        }
+    }
+}
---
+++ Code/BusinessLogic/Models/Medication.cs
@@ -0,0 +1,28 @@
+﻿namespace BusinessLogic.Models
+{
+    /// <summary>
+    /// Exposes data for a Medication 
+    /// </summary>
+    public class Medication
+    {
+        /// <summary>
+        /// Gets or sets the medication's identifier
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the medication's name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the medication's brand
+        /// </summary>
+        public string Brand { get; set; }
+
+        /// <summary>
+        /// Gets or sets the medication's price
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+}
