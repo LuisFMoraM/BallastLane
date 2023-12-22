a5746f539917ec57939604b9d71402e67cf7df95
Luis Mora <luismora02@gmail.com>
2023-12-22 01:18:40 -0500

    Add files to create/initialize the Database with the docker-compose

---
+++ Code/DataAccess/CreateDatabase/initial-db-script.sql
@@ -0,0 +1,36 @@
+﻿USE [master]
+GO
+
+IF NOT EXISTS(SELECT 1 FROM sys.databases WHERE name = 'PrescriberPoint')
+BEGIN
+	CREATE DATABASE [PrescriberPoint]
+END
+GO
+
+USE [PrescriberPoint]
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name='Medication' and xtype='U')
+BEGIN
+	CREATE TABLE [Medication] (
+		[Id]    BIGINT PRIMARY KEY IDENTITY (1, 1),
+		[Name]  VARCHAR(100) NOT NULL,
+		[Brand] VARCHAR(100) NOT NULL,
+		[Price] DECIMAL(5,2) NOT NULL,
+		CONSTRAINT UC_Medication UNIQUE ([Name], [Brand])
+	);
+END
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name='User' and xtype='U')
+BEGIN
+	CREATE TABLE [User] (
+		[Id]       BIGINT PRIMARY KEY IDENTITY (1, 1),
+		[Name]     VARCHAR(100) NOT NULL,
+		[Email]    VARCHAR(100) NOT NULL,
+		[Password] VARCHAR(MAX) NOT NULL,
+		[Phone]    VARCHAR(20)  NULL,
+		CONSTRAINT UC_User UNIQUE ([Email])
+	);
+END
+GO
