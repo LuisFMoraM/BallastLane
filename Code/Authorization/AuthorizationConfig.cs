4e29ba8b43b6d0b4b0a60e8cdb8b3f99802e3c0f
Luis Mora <luismora02@gmail.com>
2023-12-21 19:48:57 -0500

    Add the Authorization project to be able to generate JWT Tokens to work with Authorize Endpoints

---
+++ Code/Authorization/AuthorizationConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Authorization
+{
+    public class AuthorizationConfig : IAuthorizationConfig
+    {
+        /// <summary>
+        /// Implementation of the <see cref="IAuthorizationConfig.JsonWebToken"/>
+        /// </summary>
+        public string JsonWebToken()
+        {
+            var securityKey = AuthorizationConstants.SecurityKey;
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                issuer: AuthorizationConstants.SecurityTokenIssuer,
+                audience: AuthorizationConstants.SecurityTokenAudience,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: signingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Code/Authorization/AuthorizationConstants.cs
@@ -0,0 +1,23 @@
+﻿namespace Authorization
+{
+    /// <summary>
+    /// Exposes constants to work with Authorization
+    /// </summary>
+    public class AuthorizationConstants
+    {
+        /// <summary>
+        /// Gets the Security Key to Generate/Validate a JWT Token
+        /// </summary>
+        public const string SecurityKey = "245A4ECD3AAD271A8922369BAD529F78EAD4146B454F384DCED4BED136CE1C1F";
+
+        /// <summary>
+        /// Gets the Issuer to work with the Security Token
+        /// </summary>
+        public const string SecurityTokenIssuer = "PrescriberPoint";
+
+        /// <summary>
+        /// Gets the Audience to work with the Security Token
+        /// </summary>
+        public const string SecurityTokenAudience = "Readers";
+    }
+}
